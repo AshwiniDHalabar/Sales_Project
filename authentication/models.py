c07d4acb0dcb68a40c73c6a4eb96b4523014837e
Ashwini DH <ashwinidh82@gmail.com>
2022-02-21 12:25:42 +0530

    register page created

--- authentication/models.py
+++ authentication/models.py
@@ -1,12 +1,6 @@
-from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Newuser(models.Model):
-    first_name=models.CharField(max_length=50)
-    last_name=models.CharField(max_length=50)
-    email=models.CharField(max_length=150)
-    password=models.CharField(max_length=80)
-    confirmpassword=models.CharField(max_length=80)
-    phonenumber=models.IntegerField()
+
 
 
--- authentication/views.py
+++ authentication/views.py
@@ -1,34 +1,53 @@
-from sre_constants import SUCCESS
-from django.http import HttpResponse
-from django.shortcuts import render
-from authentication.models import Newuser
+from django.shortcuts import render,redirect
 from django.contrib import messages
-from django.shortcuts import redirect, render
-from server import settings
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login as login_process
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import UserRegisterForm
+from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+from django.template import Context
 
-# Create your views here.
+
+##################################################################
+####################index#######################################
 def index(request):
-    return render(request,'authentication/index.html')
+    return render(request, 'authentication/index.html',{'title':'index'})
+
+########################################################################
+########### register here #####################################
 
 def register(request):
-    if request.method=='POST':
-        first_name = request.POST.get('first_name',False)
-        last_name = request.POST.get('last_name',False)
-        email = request.POST.get('email',False)
-        password = request.POST.get('password',False)
-        confirmpassword = request.POST.get('confirmpassword',False)
-        phonenumber = request.POST.get('phonenumber',False)
-        Newuser(first_name=first_name,last_name=last_name,email=email,password=password,confirmpassword=confirmpassword,phonenumber=phonenumber).save()
-        return HttpResponse("<h1>User registration created SUCCESSFULLY</h1>")
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST) or None
+        if form.is_valid():
+            username = request.POST.get('username')
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
     else:
-        return render(request,'authentication/register.html')
+        form = UserRegisterForm()
+    return render(request, 'authentication/register.html', {'form': form,'title':'reqister here'})
+
+###################################################################################
+################login forms###################################################
 
 def login(request):
     if request.method == 'POST':
-        first_name = request.POST.get('first_name')
-        passowrd = request.POST.get('password',False)
-        return HttpResponse("<h4>login success</h4>")
 
-    else:
-        return render(request,'authentication/login.html')
+        #AuthenticationForm_can_also_be_used__
 
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            form = login(request,user)
+            messages.success(request, f' welcome {username} !!')
+            return redirect('index')
+        else:
+            messages.info(request, f'account does not exit plz sign in')
+    form = AuthenticationForm()
+    return render(request, 'authentication/login.html', {'form':form,'title':'log in'})
